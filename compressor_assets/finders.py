44115c51ee0b0a37cb62f1386b63b917a6833505
Miketsukami <temytch-s@yandex.ru>
2017-03-29 02:56:09 +0300

    Initital commit with AssetFinder, AppAssetFinder and BowerFinder

---
+++ compressor_assets/finders.py
@@ -0,0 +1,54 @@
+from collections import OrderedDict
+
+from django.conf import settings
+from django.contrib.staticfiles.finders import FileSystemFinder, AppDirectoriesFinder
+from django.core.exceptions import ImproperlyConfigured
+from django.core.files.storage import FileSystemStorage
+
+from compressor.storage import CompressorFileStorage
+
+
+class AbstractAssetFinder(FileSystemFinder):
+    def __init__(self, app_names=None, *args, **kwargs):
+        super().__init__(app_names, *args, **kwargs)
+
+        self.locations = []
+        self.storages = OrderedDict()
+
+        try:
+            paths = self.get_paths()
+        except AttributeError as e:
+            raise ImproperlyConfigured(e)
+
+        for root in paths:
+            if ('', root) not in self.locations:
+                self.locations.append(('', root))
+
+        for _, root in self.locations:
+            filesystem_storage = FileSystemStorage(location=root)
+            filesystem_storage.prefix = ''
+            self.storages[root] = filesystem_storage
+
+    def list(self, ignore_patterns):
+        return []
+
+    def get_paths(self):
+        raise NotImplemented
+
+
+class BowerFinder(AbstractAssetFinder):
+    def get_paths(self):
+        return [settings.BOWER_COMPONENTS_ROOT]
+
+
+class AssetFinder(AbstractAssetFinder):
+    def get_paths(self):
+        return [settings.COMPRESS_SOURCE_ROOT]
+
+
+class AppAssetFinder(AppDirectoriesFinder):
+    storage_class = CompressorFileStorage
+    source_dir = 'assets'
+
+    def list(self, ignore_patterns):
+        return []
---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='django-compressor-assets',
+    version='',
+    packages=['compressor_assets'],
+    url='',
+    license='MIT',
+    author='Artem Suhanov',
+    author_email='a.o.suhanov@gmail.com',
+    description='',
+    requires=[
+        'django',
+        'django-compressor'
+    ]
+)
