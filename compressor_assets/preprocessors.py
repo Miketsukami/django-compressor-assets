6d71ad5459a0d53e86aeb69a2beca01399f225dc
Miketsukami <temytch-s@yandex.ru>
2017-10-11 04:07:47 +0300

    Added interpreter option for command, i.e 'python -m' or 'bundle exec'

--- compressor_assets/preprocessors.py
+++ compressor_assets/preprocessors.py
@@ -3,28 +3,21 @@ from os import path
 
 
 class AbstractPreprocessor:
-    command_template = '{command} {parameters} {infile} {outfile}'
     command = None
-    prefix = None
+    args_template = '{parameters} {infile} {outfile}'
 
-    @staticmethod
-    def get_initial_params():
-        return {}
-
-    def __init__(self, binary_path=None, **params):
+    def __init__(self, binary_path=None, interpreter=None, **params):
         if not self.command:
             raise ImproperlyConfigured
 
-        if binary_path:
-            self.prefix = binary_path
-
-        self.params = self.get_initial_params()
-        self.params.update({
+        self.binary_path = binary_path
+        self.interpreter = interpreter
+        self.params = {
             param.replace('_', '-'): value
             for param, value in params.items()
-        })
+        }
 
-    def get_parameters(self, delimiter='='):
+    def get_parameters(self, delimiter):
         tokens = []
         for param, value in self.params.items():
             if isinstance(value, bool):
@@ -38,22 +31,34 @@ class AbstractPreprocessor:
                 raise ImproperlyConfigured
         return ' '.join(map('--{}'.format, tokens))
 
-    def get_command(self, delimiter='='):
-        return self.command_template.format(
-            command=path.join(self.prefix, self.command) if self.prefix else self.command,
+    def get_command(self):
+        command = self.command
+        if self.binary_path:
+            command = path.join(self.binary_path, command)
+        if self.interpreter:
+            command = ' '.join([self.interpreter, command])
+        return command
+
+    def get_args(self, delimiter):
+        return self.args_template.format(
             parameters=self.get_parameters(delimiter=delimiter),
             infile='{infile}',
             outfile='{outfile}',
         )
 
     def register(self, mimetype, delimiter='='):
-        return mimetype, self.get_command(delimiter)
+        return mimetype, '{command} {args}'.format(
+            command=self.get_command(),
+            args=self.get_args(delimiter)
+        )
 
 
 class Preprocessor(AbstractPreprocessor):
-    def __init__(self, command, **params):
+    def __init__(self, command, args_template=None, **kwargs):
         self.command = command
-        super().__init__(**params)
+        if args_template:
+            self.args_template = args_template
+        super().__init__(**kwargs)
 
 
 class Sass(AbstractPreprocessor):
@@ -62,17 +67,17 @@ class Sass(AbstractPreprocessor):
 
 class TypeScript(AbstractPreprocessor):
     command = 'tsc'
-    command_template = '{command} {parameters} --outFile {outfile} {infile}'
+    args_template = '{parameters} --outFile {outfile} {infile}'
 
 
 class Babel(AbstractPreprocessor):
     command = 'babel'
-    command_template = '{command} {parameters} -o {outfile} {infile}'
+    args_template = '{parameters} -o {outfile} {infile}'
 
 
 class Browserify(AbstractPreprocessor):
     command = 'browserify'
-    command_template = '{command} {infile} {parameters}'
+    args_template = '{infile} {parameters}'
 
 
 TypeScriptPreprocessor = TypeScript
